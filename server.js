84f72f9624d3651640d317afd43e903cbdc78e58
Kevin Swiber <kswiber@gmail.com>
2014-09-19 15:59:33 -0400

    Updating Zetta to latest version.

--- server.js
+++ server.js
@@ -1,21 +1,14 @@
 var zetta = require('zetta');
-var AutoScout = require('zetta-auto-scout');
 var alert = require('./alert');
 var Arm = require('./arm');
 var Heartbeat = require('./heartbeat');
 var LED = require('./led');
 var Screen = require('./screen');
 
-var HeartbeatScout = new AutoScout('heartbeat', Heartbeat);
-var LEDScout = new AutoScout('led', LED);
-var ScreenScout = new AutoScout('screen', Screen);
-var ArmScout = new AutoScout('arm', Arm);
-
 zetta()
-  .use(HeartbeatScout)
-  .use(LEDScout)
-  .use(ScreenScout)
-  .use(ArmScout)
-  //.load(alert)
-  .link('http://localhost:3004')
+  .use(Heartbeat)
+  .use(LED)
+  .use(Screen)
+  .use(Arm)
+  .load(alert)
   .listen(process.env.PORT || 1337);
